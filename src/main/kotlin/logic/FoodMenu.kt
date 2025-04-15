9b233f7a0d60289cacde49decbd94f419c9b5841
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-15 23:58:59 +0200

    data source implantation

---
+++ src/main/kotlin/logic/FoodMenu.kt
@@ -0,0 +1,23 @@
+package logic
+
+import model.MealItem
+
+interface FoodMenu {
+
+    fun filterPreparedTimeMeals(mealItems: List<MealItem>, preparedTime: Int): List<MealItem>
+    fun filterTotalFatMeals(mealItems: List<MealItem>, totalFat: Int): List<MealItem>
+    fun filterSaturatedFatMeals(mealItems: List<MealItem>, saturatedFat: Int): List<MealItem>
+    fun filterCarbohydrateMeals(mealItems: List<MealItem>, carbohydrate: Int): List<MealItem>
+    fun filterProteinMeals(mealItems: List<MealItem>, protein: Int): List<MealItem>
+    fun filterCaloriesMeals(mealItems: List<MealItem>, calories: Int): List<MealItem>
+    fun filterMealsByCountry(mealItems: List<MealItem>, country: String): List<MealItem>
+    fun getMealsByName(mealItems: List<MealItem>, foodName: String): List<MealItem>
+
+
+    fun getMealsByIngredientsNumber(mealItems: List<MealItem>, ingredientsNumbers: Int): List<MealItem>
+    fun getMealsByStepsNumber(mealItems: List<MealItem>, stepsNumber: Int): List<MealItem>
+    fun getMealByIngredient(mealItems: List<MealItem>, ingredientsName: String): MealItem
+    fun getMealByIngredients(mealItems: List<MealItem>, ingredients: List<String>): MealItem
+    fun getMealsByDate(mealItems: List<MealItem>, date: String): List<MealItem>
+
+}
