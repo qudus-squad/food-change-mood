f43c7860d21badc5761d1bb068286d1fb1cb93ee
abdo talat <Abdo.talaat740@gmail.com>
2025-04-18 11:08:22 +0200

    use data source implementation and koin for dependence injection

--- src/main/kotlin/logic/EasyFoodSuggestionUseCase.kt
+++ src/main/kotlin/logic/EasyFoodSuggestionUseCase.kt
@@ -1,66 +1,10 @@
 package logic
-
-import kotlinx.datetime.LocalDate
 import model.MealItem
-import model.Nutrition
 
-class EasyFoodSuggestionUseCase(private val dataSource: FoodChangeModeDataSource) {
+class EasyFoodSuggestionUseCase(dataSource: FoodChangeModeDataSource) {
 
     // TODO Need Implement from dataSource
-    private val meals: List<MealItem> = listOf(
-        MealItem(
-            id = 137739,
-            name = "arriba baked winter squash mexican style",
-            minutes = 55,
-            contributorId = 47892,
-            submitted = LocalDate.parse("2005-09-16"),
-            tags = listOf("60-minutes-or-less", "vegetarian"),
-            nutrition = Nutrition(
-                calories = 51.5,
-                totalFat = 0.0,
-                sugar = 13.0,
-                sodium = 0.0,
-                protein = 2.0,
-                saturatedFat = 0.0,
-                carbohydrates = 4.0
-            ),
-            stepNumbers = 11,
-            steps = listOf(
-                "make a choice and proceed",
-                "cut into half or fourths",
-                "remove seeds"
-            ),
-            description = "Spicy or sweet squash recipe.",
-            ingredients = listOf("winter squash", "mexican seasoning"),
-            ingredientNumbers = 2
-        ),
-        MealItem(
-            id = 31490,
-            name = "a bit different breakfast pizza",
-            minutes = 30,
-            contributorId = 26278,
-            submitted = LocalDate.parse("2002-06-17"),
-            tags = listOf("30-minutes-or-less", "breakfast"),
-            nutrition = Nutrition(
-                calories = 173.4,
-                totalFat = 18.0,
-                sugar = 0.0,
-                sodium = 17.0,
-                protein = 22.0,
-                saturatedFat = 35.0,
-                carbohydrates = 1.0
-            ),
-            stepNumbers = 9,
-            steps = listOf(
-                "preheat oven",
-                "press dough",
-                "bake crust"
-            ),
-            description = "Breakfast pizza that can be reheated.",
-            ingredients = listOf("prepared pizza crust", "sausage"),
-            ingredientNumbers = 2
-        )
-    )
+    private val meals = dataSource.getAllMeals()
 
     fun suggestEasyMeals(numberOfSuggest: Int): List<MealItem> {
 
