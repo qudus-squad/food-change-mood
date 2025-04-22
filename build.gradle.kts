d3a7dd9ff667656a77d3b076ec2553c76d16c008
abdo talat <Abdo.talaat740@gmail.com>
2025-04-22 14:59:33 +0200

    set up the dependencies of unit test like kotest and parameterized test and mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,17 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
     implementation("io.insert-koin:koin-core:4.0.4")
+
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:6.0.0.M3")
+    testImplementation("io.kotest:kotest-assertions-core:6.0.0.M3")
+
+    // parameterized test
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+
+    // mockk
+    testImplementation("io.mockk:mockk:1.14.0")
+
 }
 
 tasks.test {
