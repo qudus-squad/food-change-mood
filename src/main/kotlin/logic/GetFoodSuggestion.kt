5004b1cb835b3edcbdd6d5ee9a5982f4477507cf
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-22 13:21:40 +0200

    make the suggest easy meals function dynamic and remove abbreviations for number of preparation steps and num of ingredients

--- src/main/kotlin/logic/GetFoodSuggestion.kt
+++ src/main/kotlin/logic/GetFoodSuggestion.kt
@@ -1,29 +1,37 @@
 package logic
 
-import di.appModule
 import model.MealItem
-import org.koin.core.context.startKoin
-import org.koin.mp.KoinPlatform.getKoin
-import utils.Utils
 
 class GetFoodSuggestion(dataSource: FoodChangeModeDataSource) {
 
     private val meals = dataSource.getAllMeals()
 
-    fun suggestEasyMeals(numberOfSuggest: Int = 10): List<MealItem> {
+    fun suggestEasyMeals(
+        numberOfSuggestion: Int = 10,
+        preparationTime: Int = 30,
+        numberOfIngredients: Int = 5,
+        numberOfPreparationSteps: Int = 6,
+    ): List<MealItem> {
         return meals.filter { meal ->
-            isEasyMeal(meal = meal)
-        }.shuffled().take(numberOfSuggest)
+            isEasyMeal(
+                meal = meal,
+                preparationTime,
+                numberOfIngredients,
+                numberOfPreparationSteps
+            )
+        }
+            .shuffled()
+            .take(numberOfSuggestion)
     }
 
     private fun isEasyMeal(
         meal: MealItem,
-        preparationTime: Int = 30,
-        numOfIngredients: Int = 5,
-        numOfPreparationSteps: Int = 6,
+        preparationTime: Int,
+        numberOfIngredients: Int,
+        numberOfPreparationSteps: Int,
     ): Boolean {
         return meal.minutes <= preparationTime &&
-                meal.ingredients.size <= numOfIngredients &&
-                meal.steps.size <= numOfPreparationSteps
+                meal.ingredients.size <= numberOfIngredients &&
+                meal.steps.size <= numberOfPreparationSteps
     }
 }
