c9c92adb05b70f219accf9bfa9415086992ec232
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-23 20:58:34 +0200

    improve get meals suggestion use case and exception

--- src/main/kotlin/model/Exceptions.kt
+++ src/main/kotlin/model/Exceptions.kt
@@ -4,5 +4,6 @@ package model
 class InvalidCountryNameException(message: String) : Exception(message)
 
 class NoMealsFoundException(message: String) : Exception(message)
-
+class InvalidInputSuggestEasyMealsException(message: String) :Exception(message)
 class InvalidMealNumberException(message: String) : Exception(message)
+
