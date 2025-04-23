199828a3471606b237b52754718c8dbb990a82b3
Mousaber00 <mousaber00@gmail.com>
2025-04-23 14:36:28 +0200

    resolve comment review on git about hard codded value

--- src/main/kotlin/logic/GetRandomMealsWithPotatoUseCase.kt
+++ src/main/kotlin/logic/GetRandomMealsWithPotatoUseCase.kt
@@ -4,10 +4,11 @@ import model.MealItem
 
 class GetRandomMealsWithPotatoUseCase(dataSource: FoodChangeModeDataSource) {
     private val meals = dataSource.getAllMeals()
+    private val potatoKeyword = "potato"
 
     fun getPotatoMeals(randomMealNumber:Int=10): List<MealItem> {
         return meals
-            .filter { meal -> meal.ingredients.any { it.contains("potato", ignoreCase = true) } }
+            .filter { meal -> meal.ingredients.any { it.contains(potatoKeyword, ignoreCase = true) } }
             .shuffled()
             .take(randomMealNumber)
     }
